a920125ba0f9090926dffb6e975ba8bd70ce1221
Muhamad Diaz R <muhamaddiaz10@gmail.com>
2017-11-05 13:55:01 +0700

    CV update

--- js/main.js
+++ js/main.js
@@ -4,34 +4,34 @@ $(document).ready(function() {
     $('.timelines-c').click(function() {
         $('.home, .message,.message-content').hide('slow');
         $('.timelines').show('slow');
-        $('.timelines-c').addClass('aktif');
-        $('.home-c, .message-c').removeClass('aktif');
+        $('.timelines-c').addClass('active');
+        $('.home-c, .message-c').removeClass('active');
     });
     $('.home-c').click(function() {
         $('.home').show('slow');
         $('.timelines, .message, .message-content').hide('slow');
-        $('.home-c').addClass('aktif');
-        $('.timelines-c, .message-c').removeClass('aktif');
+        $('.home-c').addClass('active');
+        $('.timelines-c, .message-c').removeClass('active');
     });
     $('.message-c').click(function() {
         $('.home, .timelines, .message-content').hide('slow');
         $('.message, .message-main').show('slow');
-        $('.message-c').addClass('aktif');
-        $('.timelines-c, .home-c').removeClass('aktif');
+        $('.message-c').addClass('active');
+        $('.timelines-c, .home-c').removeClass('active');
     });
     $('#message-content').click(function() {
         $('.home, .timelines').hide('slow');
         $('.message-main').slideUp('slow');
         $('.message-content').slideDown('slow');
-        $(this).addClass('aktif');
-        $('.timelines-c, .home-c').removeClass('aktif');
+        $(this).addClass('active');
+        $('.timelines-c, .home-c').removeClass('active');
     });
     $('#top-button').click(function() {
         $('.top-nav-menu').slideToggle('slow');
     });
     $('.bt').click(function() {
         $('.main-content').fadeToggle('slow');
-        $('.not-toggle').fadeToggle('fast');
+        $('.not-toggle').fadeToggle('active');
     })
     $('.bts').click(function() {
         $('.main-content').toggleClass('full-scr');
